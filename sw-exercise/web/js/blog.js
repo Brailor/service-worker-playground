b45332328b09085620601c57f26df2d4cde97e12
Viktor Ohad <ygatravaz@protonmail.com>
2019-05-12 11:47:43 +0200

    add messaging between client, sw

--- sw-exercise/web/js/blog.js
+++ sw-exercise/web/js/blog.js
@@ -1,11 +1,16 @@
 (function Blog() {
   'use strict';
 
-  var offlineIcon;
-  var isOnline = 'onLine' in navigator ? navigator.onLine : true;
-  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '');
+  let offlineIcon;
+  let isOnline = 'onLine' in navigator ? navigator.onLine : true;
+  let isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '');
+  let usingSW = 'serviceWorker' in navigator;
+  let swRegistration;
+  let svcWorker;
 
   document.addEventListener('DOMContentLoaded', ready, false);
+
+  initServiceWorker().catch(console.error);
 
   // **********************************
 
@@ -19,11 +24,52 @@
     window.addEventListener('online', () => {
       offlineIcon.classList.add('hidden');
       isOnline = true;
+      sendStatusUdpdate();
     });
 
     window.addEventListener('offline', () => {
       offlineIcon.classList.remove('hidden');
       isOnline = false;
+      sendStatusUdpdate();
     });
+  }
+
+  async function initServiceWorker() {
+    swRegistration = await navigator.serviceWorker.register('/sw.js', {
+      updateViaCache: 'none'
+    });
+
+    svcWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
+    sendStatusUdpdate(svcWorker);
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      svcWorker = navigator.serviceWorker.controller;
+      sendStatusUdpdate(svcWorker);
+    });
+
+    navigator.serviceWorker.addEventListener('message', onSWMessage);
+  }
+
+  function onSWMessage(event) {
+    let { data } = event;
+    if (data.requestStatusUpdate) {
+      sendStatusUdpdate(event.ports && event.ports[0]);
+    }
+  }
+
+  function sendStatusUdpdate(target) {
+    console.log(`Recieved status update req from SW, responding... isOnline: ${isOnline}, isLoggedIn: ${isLoggedIn}`);
+
+    sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } }, target);
+  }
+
+  function sendSWMessage(msg, target) {
+    if (target) {
+      target.postMessage(msg);
+    } else if (svcWorker) {
+      svcWorker.postMessage(msg);
+    } else {
+      navigator.serviceWorker.controller.postMessage(msg);
+    }
   }
 })();
--- sw-exercise/web/js/sw.js
+++ sw-exercise/web/js/sw.js
@@ -1,3 +1,50 @@
-"use strict";
+'use strict';
 
-// TODO
+const version = 2;
+let isOnline = true;
+let isLoggedIn = false;
+
+self.addEventListener('install', onInstall);
+self.addEventListener('activate', onActivate);
+self.addEventListener('message', onMessage);
+
+main().catch(console.error);
+
+async function onInstall(event) {
+  console.log(`Service Worker ${version} is installed.`);
+  self.skipWaiting();
+}
+async function onActivate(event) {
+  event.waitUntil(handleActivation());
+}
+
+async function main() {
+  await sendMessage({ requestStatusUpdate: true });
+}
+
+async function handleActivation(params) {
+  await clients.claim();
+  console.log(`Service Worker ${version} is activated.`);
+}
+
+async function sendMessage(msg) {
+  let allClients = await clients.matchAll({ includeUncontrolled: true });
+
+  return Promise.all(
+    allClients.map(function clientMsg(client) {
+      let channel = new MessageChannel();
+
+      channel.port1.onmessage = onMessage;
+      return client.postMessage(msg, [channel.port2]);
+    })
+  );
+}
+
+async function onMessage(event) {
+  let { data } = event;
+
+  if (data.statusUpdate) {
+    ({ isLoggedIn, isOnline } = data.statusUpdate);
+    console.log(`SW (v${version}), status update... isOnline: ${isOnline}, isLoggedIn: ${isLoggedIn}`);
+  }
+}
